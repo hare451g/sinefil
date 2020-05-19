20ea509b98a57028a7eb1aad6dc31a30f1a257bd
hendra sadewa <hendrasadewa30@gmail.com>
2020-05-20 04:32:45 +0700

    install thunk

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,13 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
 import rootReducer from './rootReducer';
 
-const composeEnhancer = composeWithDevTools({
-  name: 'Sinefil',
-});
+const middlwares = [thunk];
 
-const store = createStore(rootReducer, composeEnhancer());
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(...middlwares)),
+);
 
 export default store;
