3e5bd54c11048189cf6c394b4fbcf8f9437b9fd7
hendra sadewa <hendrasadewa30@gmail.com>
2020-05-19 17:29:15 +0700

    add not found page

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -4,12 +4,14 @@ import { Router } from '@reach/router';
 // containers | Pages
 import DetailPage from '../DetailPage';
 import HomePage from '../HomePage';
+import NotFoundPage from '../NotFoundPage';
 
 function App() {
   return (
     <Router>
       <DetailPage path="/details/:id" />
       <HomePage path="/" />
+      <NotFoundPage path="*" />
     </Router>
   );
 }
