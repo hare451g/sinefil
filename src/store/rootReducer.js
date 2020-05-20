bf497133680d3e66277189bf45b012c0b8d695b2
hendra sadewa <hendrasadewa30@gmail.com>
2020-05-20 15:56:42 +0700

    register details to root reducer

--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import movieReducer from './movies';
+import detailsReducer from './details';
 
 export default combineReducers({
   movies: movieReducer,
+  details: detailsReducer,
 });
